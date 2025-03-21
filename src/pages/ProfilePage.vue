c0c651de0602d1d7e236ccd828d4a117f535d6cc
Roman Wernsing <wernsingroman@gmail.com>
2025-03-21 16:58:21 -0600

    more styling on the profile card

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -34,41 +34,63 @@ async function getProfileById() {
       <div v-if="profile" class="col-12">
         <div class="d-flex justify-content-center bg-img my-3 rounded-4"
           :style="{ backgroundImage: `url(${profile.coverImg})` }">
-          <div class="d-flex flex-column">
-            <img class="profile-img mt-5 mb-3" :src="profile.picture" alt="">
+          <div class="d-flex flex-column text-light text-shadow">
+            <h2 class="text-center mt-2 fs-1">
+              {{ profile.name }}
+            </h2>
+            <div class="d-flex justify-content-center">
+              <img class="rounded-img my-3" :src="profile.picture" alt="">
+            </div>
             <span class="text-center fs-1">
-              <a v-if="profile.linkedin" class="text-dark" :title="`link to ${profile.name}'s linkedin`" target="_blank"
+              <a v-if="profile.linkedin" :title="`link to ${profile.name}'s linkedin`" target="_blank"
                 :href="profile.linkedin">
-                <span class="mdi mdi-linkedin"></span>
+                <span class="mdi mdi-linkedin text-light"></span>
               </a>
-              <a v-if="profile.github" class="text-dark" :title="`link to ${profile.name}'s github`" target="_blank"
+              <a v-if="profile.github" :title="`link to ${profile.name}'s github`" target="_blank"
                 :href="profile.github">
-                <span class="mdi mdi-github"></span>
+                <span class="mdi mdi-github text-light"></span>
               </a>
-              <a v-if="profile.resume" class="text-dark" :title="`link to ${profile.name}'s resume`" target="_blank"
+              <a v-if="profile.resume" :title="`link to ${profile.name}'s resume`" target="_blank"
                 :href="profile.resume">
-                <span class="mdi mdi-file-account"></span>
+                <span class="mdi mdi-file-account text-light"></span>
               </a>
             </span>
+            <p class="text-center fs-5">
+              {{ profile.class }}
+            </p>
+            <div class="text-center fs-4">
+              <b>
+                {{ profile.bio }}
+              </b>
+            </div>
           </div>
+        </div>
+        <div>
         </div>
       </div>
     </div>
+
   </section>
 </template>
 
 
 <style lang="scss" scoped>
-.profile-img {
+img {
   aspect-ratio: 1/1;
   border-radius: 50%;
   object-fit: cover;
   height: 30dvh;
+  width: 30dvh;
+
 }
 
 .bg-img {
   background-position: center;
   background-size: cover;
   min-height: 70dvh;
+}
+
+.text-shadow {
+  text-shadow: 1px 1px 5px rgb(255, 109, 221);
 }
 </style>
